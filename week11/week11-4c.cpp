1a44b2f79ef60b9cfda397fff53d18d0576874fb
Lee-Z-Han <cindy20050116@gmail.com>
2024-05-03 11:58:44 +0800

    week11

---
+++ week11/week11-4c.cpp
@@ -0,0 +1,20 @@
+// week11-4c.cpp Part 3
+#include <stdio.h>
+int main()
+{
+	int a[30][30];
+	int m,n; // Part 1 : Input
+	scanf("%d%d",&m,&n);
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("\n");
+	for(int j=0;j<n;j++){// Part 2 : Output
+		for(int i=m-1;i>=0;i--){ // Part3
+			printf("%2d ",a[i][j]);
+		}
+		printf("\n");
+	}
+}
